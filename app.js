6a7ace6f81a05d7805c1f70998b69d44639892e3
Rudra Sanandiya <rudrasanandiyards@gmail.com>
2026-01-17 18:47:16 +0530

    Add listing model and checked its functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
 const MONGO_URL = 'mongodb://127.0.0.1:27017/heavenly';
+const Listing = require('./models/listing');
 
 main()
 .then(() => console.log('Successfully Connected to MongoDB'))
@@ -14,6 +15,24 @@ async function main() {
 
 app.get('/', (req, res) => {
     res.send('Hello, server is up and running!');
+});
+
+// demo route to create a listing
+app.get('/listing', async (req, res) => {
+    const newListing = new Listing({
+        title: 'Cozy Cottage',
+        description: 'A cozy cottage in the countryside.',
+        image: '',
+        price: 150,
+        location: 'Countryside'
+    });
+
+    try {
+        const savedListing = await newListing.save();
+        res.json(savedListing);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 app.listen(port,()=>{
---
+++ models/listing.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: { 
+        type: String,
+        required: true 
+    },
+    description: { 
+        type: String,
+        required: true 
+    },
+    image: {
+        type: String,
+        default: 'https://www.pexels.com/photo/a-blank-banner-on-a-brick-wall-12883028/',
+        set: (url) => url === '' ? 'https://www.pexels.com/photo/a-blank-banner-on-a-brick-wall-12883028/' : url
+    },
+    price: { 
+        type: Number, 
+        required: true 
+    },
+    location: { 
+        type: String, 
+        required: true 
+    }
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+module.exports = Listing;
