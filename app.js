556a53a69d41a0b35d5369bdc639aa2dd05de7d1
Rudra Sanandiya <rudrasanandiyards@gmail.com>
2026-01-17 18:11:59 +0530

    config: setup MongoDB connection using mongoose

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const mongoose = require('mongoose');
+const MONGO_URL = 'mongodb://127.0.0.1:27017/heavenly';
+
+main()
+.then(() => console.log('Successfully Connected to MongoDB'))
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, server is up and running!');
