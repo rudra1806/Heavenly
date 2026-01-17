739055ca186b012aacb9ae48515c672c36f73084
Rudra Sanandiya <rudrasanandiyards@gmail.com>
2026-01-17 22:45:43 +0530

    add single listing route for each properties and refactored listings page

--- app.js
+++ app.js
@@ -17,16 +17,33 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello, server is up and running!');
 });
 
+// Listings Route
 app.get('/listings', async (req, res) => {
     try{
         let allListings = await Listing.find({});
         res.render('listings/index.ejs', { listings: allListings });
     }catch(err){
         res.status(500).send('Error retrieving listings');
+    }
+});
+
+// Single Listing Route
+app.get('/listings/:id', async (req, res) => {
+    let id = req.params.id;
+    try{
+        let listing = await Listing.findById(id);
+        if(listing){
+            res.render('listings/show.ejs', { listing: listing });
+        }else{
+            res.status(404).send('Listing not found');
+        }
+    }catch(err){
+        res.status(500).send('Error retrieving listing');
     }
 });
 
